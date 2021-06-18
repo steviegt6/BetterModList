7d724f4f579d28fc474dd3ec7b20275721d838bc
Stevie <xxlennygamerxx@gmail.com>
2021-06-17 19:15:17 -0700

    reflection utils

---
+++ Common/Utilities/IDs/ModdedInterfaceIDs.cs
@@ -0,0 +1,31 @@
+﻿namespace BetterModList.Common.Utilities.IDs
+{
+    /// <summary>
+    ///     IDs corresponding to modded menu IDs.
+    /// </summary>
+    public static class ModdedInterfaceIDs
+    {
+        public const int ModsMenuID = 10000;
+        public const int ModSourcesID = 10001;
+        public const int LoadModsID = 10002;
+        public const int BuildModID = 10003;
+        public const int ErrorMessageID = 10005;
+        public const int ReloadModsID = 10006;
+        public const int ModBrowserID = 10007;
+        public const int ModInfoID = 10008;
+        public const int ManagePublishedID = 10011;
+        public const int UpdateMessageID = 10012;
+        public const int InfoMessageID = 10013;
+        public const int EnterPassPhraseMenuID = 10015;
+        public const int ModPacksMenuID = 10016;
+        public const int ModLoaderSettingsID = 10017;
+        public const int EnterSteamIDMenuID = 10018;
+        public const int ExtractModID = 10019;
+        public const int DownloadProgressID = 10020;
+        public const int DeveloperModeHelpID = 10022;
+        public const int ProgressID = 10023;
+        public const int ModConfigID = 10024;
+        public const int CreateModID = 10025;
+        public const int ExitID = 10026;
+	}
+}
---
+++ Common/Utilities/IDs/ModdedInterfaceInstances.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using Terraria.ModLoader;
+using Terraria.ModLoader.UI;
+
+namespace BetterModList.Common.Utilities.IDs
+{
+    /// <summary>
+    ///     UI-state instances.
+    /// </summary>
+    public static class ModdedInterfaceInstances
+    {
+        private static readonly Assembly ModAssembly = typeof(ModLoader).Assembly;
+        private static readonly Type InterfaceType = ModAssembly.GetCachedType("Terraria.ModLoader.UI.Interface");
+
+        public static object ModsMenu => InterfaceType.GetField("modsMenu").GetValue(null);
+
+        public static object LoadMods => InterfaceType.GetField("loadMods").GetValue(null);
+
+        public static object ModSources => InterfaceType.GetField("modSources").GetValue(null);
+
+        public static object BuildMod => InterfaceType.GetField("buildMod").GetValue(null);
+
+        public static object ErrorMessage => InterfaceType.GetField("errorMessage").GetValue(null);
+
+        public static object ModBrowser => InterfaceType.GetField("modBrowser").GetValue(null);
+
+        public static object ModInfo => InterfaceType.GetField("modInfo").GetValue(null);
+
+        public static object ManagePublished => InterfaceType.GetField("managePublished").GetValue(null);
+
+        public static object UpdateMessage => InterfaceType.GetField("updateMessage").GetValue(null);
+
+        public static object InfoMessage => InterfaceType.GetField("infoMessage").GetValue(null);
+
+        public static object EnterPassPhraseMenu => InterfaceType.GetField("enterPassphraseMenu").GetValue(null);
+
+        public static object ModPacksMenu => InterfaceType.GetField("modPacksMenu").GetValue(null);
+
+        public static object EnterSteamIDMenu => InterfaceType.GetField("enterSteamIDMenu").GetValue(null);
+
+        public static object ExtractMod => InterfaceType.GetField("extractMod").GetValue(null);
+
+        public static object DeveloperModeHelp => InterfaceType.GetField("developerModeHelp").GetValue(null);
+
+        public static object ModConfig => InterfaceType.GetField("modConfig").GetValue(null);
+
+        public static object ModConfigList => InterfaceType.GetField("modConfigList").GetValue(null);
+
+        public static object CreateMod => InterfaceType.GetField("createMod").GetValue(null);
+
+        public static object Progress => InterfaceType.GetField("progress").GetValue(null);
+
+        public static object DownloadProgress => InterfaceType.GetField("downloadProgress").GetValue(null);
+    }
+}
